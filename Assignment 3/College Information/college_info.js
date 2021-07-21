db1c3a79b662c4649a1f980a53032a8325b2d8c8
Eli6th <57406743+Eli6th@users.noreply.github.com>
2021-07-20 21:44:42 -0400

    Assignment 3 almost finised college

--- Assignment 3/College Information/college_info.js
+++ Assignment 3/College Information/college_info.js
@@ -18,37 +18,86 @@ function display(display_schools) {
     for (var school = 0; school < display_schools.length; school++) {
         var newRow = college_table.insertRow(college_table.rows.length);
         newRow.classList.add('zebra');
-        newRow.insertCell(0).innerHTML = display_schools[school].nickname;
-        newRow.insertCell(1).innerHTML = display_schools[school].SATh;
-        newRow.insertCell(2).innerHTML = display_schools[school].SATl;
-        newRow.insertCell(3).innerHTML = display_schools[school].tuition;
+
+        var nickname = newRow.insertCell(0);
+        var SAT_high = newRow.insertCell(1);
+        var SAT_low = newRow.insertCell(2);
+        var tuition = newRow.insertCell(3);
+
+        nickname.classList.add('college_point');
+        SAT_high.classList.add('college_point');
+        SAT_low.classList.add('college_point');
+        tuition.classList.add('college_point');
+        tuition.classList.add('price');
+
+        nickname.innerHTML = display_schools[school].nickname;
+        SAT_high.innerHTML = display_schools[school].SATh;
+        SAT_low.innerHTML = display_schools[school].SATl;
+        tuition.innerHTML = display_schools[school].tuition;
     }
-    format(college_table);
 }
 function deleteTableRows(start, end, table) {
     for (var i = start; i < end; i++) {
         table.deleteRow(start);
     }
 }
-function format(college_table) {
-    var college_rows = document.getElementsByClassName("zebra");
 
-    for (var i = 0; i < college_rows.length; i++) {
-        college_rows[i].style.borderLeft = "1px solid black";
-        college_rows[i].style.borderRight = "1px solid black";
+function compare(chosenSchoolType, max_tuition, SAT_high, SAT_low) {
+    var display_schools = [];
+    for (var i = 0; i < univArray.length; i++) {
+        var shouldAdd = true;
+        if (chosenSchoolType != undefined && chosenSchoolType != 'dc' && chosenSchoolType != univArray[i].ownership) {
+            alert("Stopped 1");
+            shouldAdd = false;
+        }
+
+        if (max_tuition != "" && max_tuition <= univArray[i].tuition) {
+            alert("Stopped 2");
+            shouldAdd = false;
+        }
+                
+        if (SAT_high != "" && SAT_high <= univArray[i].SATh) {
+            alert("Stopped 3");
+            shouldAdd = false;
+        }
+
+        if (SAT_low != "" && SAT_low >= univArray[i].SATl) {
+            alert("Stopped 4");
+            shouldAdd = false;
+        }
+        
+        if (shouldAdd) {
+            display_schools.push(univArray[i]);
+        }
     }
+
+    return display_schools;
 }
-function readInputs() {
+
+function search() {
     var school_types = document.getElementsByName('school_type');
     var chosenSchoolType;
     for (var i = 0; i < school_types.length; i++) {
         if (school_types[i].checked) {
-            chosenSchoolType = school_types[i];
+            chosenSchoolType = school_types[i].id;
+            alert(chosenSchoolType);
         }
     }
 
+    var max_tuition = document.getElementById('max_tuition').value;
+    var fMax_tuition = parseFloat(max_tuition);
 
-}
-function search() {
+    var SAT_high = document.getElementById('max_high_sat').value;
+    var fSAT_high = parseFloat(SAT_high);
 
+    var SAT_low = document.getElementById('max_low_sat').value;
+    var fSAT_low = parseFloat(SAT_low);
+
+    alert("Max tuition: " + max_tuition);
+    alert("High SAT: " + SAT_high);
+    alert("Low SATn: " + SAT_low);
+    alert("School Type: " + chosenSchoolType);
+
+    var display_schools = compare(chosenSchoolType, fMax_tuition, fSAT_high, fSAT_low);
+    display(display_schools);
 }
